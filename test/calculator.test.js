26e092a39ff42d6d9998c6c44980c9714da82bc7
richard <richard@ifocs.com>
2021-07-10 17:44:47 +0100

    revert

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -27,7 +27,7 @@ describe('calculate()', () => {
   it('should return rounded down numbers for floating decimal results', () => {
     expect(calculate(42067, propertyTypes.RESIDENTIAL, countries.SCOTLAND, buyerTypes.INVESTOR).tax).to.be.equal(1262);
     expect(calculate(142066, propertyTypes.RESIDENTIAL, countries.WALES, buyerTypes.INVESTOR).tax).to.be.equal(4261);
-    expect(calculate(542524, propertyTypes.RESIDENTIAL, countries.ENGLAND, buyerTypes.INVESTOR).tax).to.be.equal(18401);
+    expect(calculate(542524, propertyTypes.RESIDENTIAL, countries.ENGLAND, buyerTypes.INVESTOR).tax).to.be.equal(30901);
     expect(calculate(2042067, propertyTypes.COMMERCIAL, countries.ENGLAND, buyerTypes.INVESTOR).tax).to.be.equal(91603);
     expect(calculate(2000237, propertyTypes.RESIDENTIAL, countries.ENGLAND, buyerTypes.MOVING_HOUSE).summaryBands[3].taxAdded).to.be.equal(60028);
   });
@@ -60,26 +60,24 @@ describe('calculate()', () => {
     const sdResult = calculate(2500000, propertyTypes.RESIDENTIAL, countries.ENGLAND, buyerTypes.FIRST_TIME);
     console.log(sdResult);
     expect(sdResult.ok).to.be.equal('ok');
-    expect(sdResult.tax).to.be.equal(198750);
+    expect(sdResult.tax).to.be.equal(211250);
     expect(sdResult.propertyValue).to.be.equal(2500000);
     expect(sdResult.buyerType).to.be.equal('first');
     expect(sdResult.summaryBands.length).to.be.equal(4);
     expect(sdResult.summaryBands[0].start).to.be.equal(0);
     expect(sdResult.summaryBands[0].taxAdded).to.be.equal(0);
-    expect(sdResult.summaryBands[1].taxAdded).to.be.equal(21250);
+    expect(sdResult.summaryBands[1].taxAdded).to.be.equal(33750);
     expect(sdResult.summaryBands[2].taxAdded).to.be.equal(57500);
     expect(sdResult.summaryBands[3].taxAdded).to.be.equal(120000);
     expect(sdResult.summaryBands[3].bandLimit).to.be.equal('+++')
   });
 });
 
-
-
 describe('calculate()', () => {
   it('should return correct values for first time buyers', () => {
     expect(calculate(250000, propertyTypes.RESIDENTIAL, countries.ENGLAND, buyerTypes.FIRST_TIME).tax).to.be.equal(0);
-    expect(calculate(350000, propertyTypes.RESIDENTIAL, countries.ENGLAND, buyerTypes.FIRST_TIME).tax).to.be.equal(0);
-    expect(calculate(550000, propertyTypes.RESIDENTIAL, countries.ENGLAND, buyerTypes.FIRST_TIME).tax).to.be.equal(2500);
+    expect(calculate(350000, propertyTypes.RESIDENTIAL, countries.ENGLAND, buyerTypes.FIRST_TIME).tax).to.be.equal(2500);
+    expect(calculate(550000, propertyTypes.RESIDENTIAL, countries.ENGLAND, buyerTypes.FIRST_TIME).tax).to.be.equal(15000);
     expect(calculate(170000, propertyTypes.RESIDENTIAL, countries.SCOTLAND, buyerTypes.FIRST_TIME).tax).to.be.equal(0);
     expect(calculate(250000, propertyTypes.RESIDENTIAL, countries.SCOTLAND, buyerTypes.FIRST_TIME).tax).to.be.equal(1500);
     expect(calculate(170000, propertyTypes.RESIDENTIAL, countries.WALES, buyerTypes.FIRST_TIME).tax).to.be.equal(0);
@@ -90,8 +88,8 @@ describe('calculate()', () => {
 describe('calculate()', () => {
   it('should return correct values for moving home', () => {
     expect(calculate(250000, propertyTypes.RESIDENTIAL, countries.ENGLAND, buyerTypes.MOVING_HOUSE).tax).to.be.equal(0);
-    expect(calculate(350000, propertyTypes.RESIDENTIAL, countries.ENGLAND, buyerTypes.MOVING_HOUSE).tax).to.be.equal(0);
-    expect(calculate(550000, propertyTypes.RESIDENTIAL, countries.ENGLAND, buyerTypes.MOVING_HOUSE).tax).to.be.equal(2500);
+    expect(calculate(350000, propertyTypes.RESIDENTIAL, countries.ENGLAND, buyerTypes.MOVING_HOUSE).tax).to.be.equal(5000);
+    expect(calculate(550000, propertyTypes.RESIDENTIAL, countries.ENGLAND, buyerTypes.MOVING_HOUSE).tax).to.be.equal(15000);
     expect(calculate(550000, propertyTypes.RESIDENTIAL, countries.ENGLAND, buyerTypes.MOVING_HOUSE).taxName.short).to.be.equal('SDLT');
     expect(calculate(170000, propertyTypes.RESIDENTIAL, countries.SCOTLAND, buyerTypes.MOVING_HOUSE).tax).to.be.equal(500);
     expect(calculate(250000, propertyTypes.RESIDENTIAL, countries.SCOTLAND, buyerTypes.MOVING_HOUSE).tax).to.be.equal(2100);
@@ -120,15 +118,15 @@ describe('calculate()', () => {
     const expectedValues = [
       3000,
       6000,
-      9000,
-      12000,
-      15000,
-      23000,
-      31000,
-      39000,
-      47000,
-      58750,
-      198750,
+      11500,
+      19500,
+      27500,
+      35500,
+      43500,
+      51500,
+      59500,
+      71250,
+      211250,
     ];
     testValues.forEach((v, idx) => {
       expect(calculate(v, propertyTypes.RESIDENTIAL, countries.ENGLAND, buyerTypes.INVESTOR).tax).to.be.equal(expectedValues[idx]);
